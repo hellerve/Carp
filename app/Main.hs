6a94bbac257bf925ae467caa2d9c4e4d446c5eb1
hellerve <veit@veitheller.de>
2018-01-25 16:14:54 +0100

    core: rewrite endo-map in carp using unsafe-nth

--- app/Main.hs
+++ app/Main.hs
@@ -116,7 +116,6 @@ arrayModule :: Env
 arrayModule = Env { envBindings = bindings, envParent = Nothing, envModuleName = Just "Array", envUseModules = [], envMode = ExternalEnv }
   where bindings = Map.fromList [ templateNth
                                 , templateAllocate
-                                , templateEMap
                                 , templateFilter
                                 , templateRaw
                                 , templateAset
