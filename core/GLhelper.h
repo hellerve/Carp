f87ab4d05290d4ce05fa53f10dce41cb5da10b8a
hellerve <veit@veitheller.de>
2019-06-14 14:36:33 +0200

    core: initial gl rework

---
+++ core/GLhelper.h
@@ -0,0 +1,15 @@
+GLsizei GL_to_MINUS_sizei(int i) {
+  return (GLsizei) i;
+}
+
+GLsizeiptr GL_to_MINUS_sizeiptr(int i) {
+  return (GLsizeiptr) i;
+}
+
+GLuint GL_to_MINUS_uint(int i) {
+  return (GLuint) i;
+}
+
+GLuint GLuint_copy(GLuint* i) {
+  return *i;
+}
